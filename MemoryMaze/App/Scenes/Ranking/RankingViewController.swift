485c51d5aa3618a1d0867e673a709cee9f3ad9b2
Diggo Silva <rdiggosilva@gmail.com>
2025-01-19 13:10:16 -0300

    Implemented ranking view

--- MemoryMaze/App/Scenes/Ranking/RankingViewController.swift
+++ MemoryMaze/App/Scenes/Ranking/RankingViewController.swift
@@ -5,4 +5,48 @@
 //  Created by Diggo Silva on 19/01/25.
 //
 
-import Foundation
+import UIKit
+
+class RankingViewController: UIViewController {
+    
+    let rankingView = RankingView()
+    let viewModel = RankingViewModel()
+    
+    override func loadView() {
+        super.loadView()
+        view = rankingView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setNavBar()
+        setDelegatesAndDataSources()
+    }
+    
+    private func setNavBar() {
+        title = "Melhores pontuações"
+    }
+    
+    private func setDelegatesAndDataSources() {
+        rankingView.tableView.delegate = self
+        rankingView.tableView.dataSource = self
+        rankingView.tableView.reloadData()
+    }
+}
+
+extension RankingViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfRowsInSection()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TableCell.identifier, for: indexPath) as? TableCell else { return UITableViewCell() }
+        cell.rankPositionLabel.text = "\(indexPath.row + 1)"
+        cell.configure(player: viewModel.cellForRowAt(indexPath: indexPath))
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
