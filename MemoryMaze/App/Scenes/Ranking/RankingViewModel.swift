be17fc303e2dc53fad8ef0787db283a06aea2bc8
Diggo Silva <rdiggosilva@gmail.com>
2025-01-20 09:24:52 -0300

    Created ranking viewModel

--- MemoryMaze/App/Scenes/Ranking/RankingViewModel.swift
+++ MemoryMaze/App/Scenes/Ranking/RankingViewModel.swift
@@ -7,21 +7,44 @@
 
 import Foundation
 
-class RankingViewModel {
-    var rankingList: [Player] = [
-        Player(name: "Jess", score: 440),
-        Player(name: "Ivete", score: 520),
-        Player(name: "Diggo", score: 480),
-        Player(name: "Junior", score: 560),
-        Player(name: "Pitoquinho", score: 600),
-    ]
+protocol RankingViewModelProtocol {
+    func saveScorePlayer(name: String, score: Int)
+    func numberOfRowsInSection() -> Int
+    func cellForRowAt(indexPath: IndexPath) -> Player
+    func loadPlayerList()
+    var rankingList: [Player] { get }
+    var rankingListTop6: [Player] { get }
+}
+
+class RankingViewModel: RankingViewModelProtocol {
+    private var repository = Repository()
+    var rankingList: [Player] = []
+    var rankingListTop6: [Player] = []
+    
+    init(repository: Repository = Repository()) {
+        self.repository = repository
+    }
     
     func numberOfRowsInSection() -> Int {
-        return rankingList.count
+        return rankingListTop6.count
     }
     
     func cellForRowAt(indexPath: IndexPath) -> Player {
-        rankingList = rankingList.sorted { $0.score > $1.score }
-        return rankingList[indexPath.row]
+        rankingListTop6 = rankingList.sorted { $0.score > $1.score }
+        return rankingListTop6[indexPath.row]
+    }
+    
+    func saveScorePlayer(name: String, score: Int) {
+        let newPlayer = Player(name: name, score: score)
+        rankingList.append(newPlayer)
+        let top6 = rankingList.sorted { $0.score > $1.score }
+        rankingListTop6 = Array(top6.prefix(6))
+        repository.save(player: newPlayer)
+    }
+    
+    func loadPlayerList() {
+        rankingList = repository.loadPlayerList()
+        let top6 = rankingList.sorted { $0.score > $1.score }
+        rankingListTop6 = Array(top6.prefix(6))
     }
 }
